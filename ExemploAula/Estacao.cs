b87ff64a49cc410fa2b2cc6fb276b5c80b7a3a1c
Wellignton <welltecnic@hotmail.com>
2026-09-08 15:47:42 -0300

    feat: Atribuitos da classe Estacao

---
+++ ExemploAula/Estacao.cs
@@ -0,0 +1,12 @@
+﻿namespace ExemploAula
+{
+    class Estacao
+    {
+        //PROPRIEDADES (ATRIBUTOS)
+
+        public string Motorista { get; set; }
+        public double CapacidadeKwh {get;set;}
+        public double  PorcentagemAtual { get; set; }
+        public double PotenciaKW { get; set; }
+    }
+}
